4df1cb1993d51a720dbae6196de73682badcfc7f
sahuashi <54184879+sahuashi@users.noreply.github.com>
2021-03-14 12:52:15 -0400

    start project profile pages

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function reset(){
     document.getElementById("projects").hidden = true;
     document.getElementById("contact").hidden = true;
 }
+
+function showProjectProfile(id){
+    hideProjects(true);
+    document.getElementById(id).hidden=false;
+}
+
+function hideProjects(boolean) {
+    [...document.getElementsByClassName("project")].forEach(element => element.hidden = boolean);
+}
+
+function goBack(){
+    [...document.getElementsByClassName("profile")].forEach(element => element.hidden = true);
+    hideProjects(false);
+}
