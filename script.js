abb3ac4ad181e7a1a383b39eb45d227b2d574b2a
sahuashi <54184879+sahuashi@users.noreply.github.com>
2021-11-22 22:22:23 -0500

    feat: add dark/light mode toggle

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+toggleDarkMode = () => {
+  let body = document.body;
+  body.classList.toggle("dark-mode");
+  let btn = document.getElementById("moon");
+  btn.innerText = body.classList.contains("dark-mode")? '🌕' : '🌑';
+}
